691549cc2c1ddac5ae1e917fa147e9a91ebb3637
Fabian Zamudio <fabian.zamudio.89@gmail.com>
2016-06-21 13:40:15 -0300

    Se agrega driver para pg

--- controllers/home.js
+++ controllers/home.js
@@ -1,24 +1,70 @@
+/**********************************
+**	REQUIRES
+***********************************/
 var fs = require('fs');
 var path = require("path");
 var handlebars = require('handlebars');
+var pg = require('pg');
+var conf = require('../core/config.js');
+
+/**********************************
+**	AUX
+***********************************/
+
+
 
 /**********************************
 **	HOME
 ***********************************/
 exports.home = function(req, res) {
 	var data = {};
-	fs.readFile(path.resolve(__dirname, '../views/index.html'),'utf-8','utf-8', function (error, source) {
-		if (error) {
-			require('../controllers/404').get(req, res);
-		}
-		res.writeHead(200, {
-			'Content-Type': 'text/html'
+	pg.connect(conf.conectionString(), function(err, client, done) {
+
+	    var handleError = function(err) {
+	      // no error occurred, continue with the request
+	      if(!err) return false;
+
+	      // An error occurred, remove the client from the connection pool.
+	      // A truthy value passed to done will remove the connection from the pool
+	      // instead of simply returning it to be reused.
+	      // In this case, if we have successfully received a client (truthy)
+	      // then it will be removed from the pool.
+	      if(client){
+	        done(client);
+	      }
+	      res.writeHead(500, {'content-type': 'text/plain'});
+	      res.end('An error occurred');
+	      return true;
+	    };
+
+	    // handle an error from the connection
+	    if(handleError(err)) return;
+
+		client.query('SELECT * FROM users', function(err, result) {
+
+		  // handle an error from the query
+		  if(handleError(err)) return;
+
+		  // return the client to the connection pool for other requests to reuse
+		  done();
+		  fs.readFile(path.resolve(__dirname, '../views/index.html'),'utf-8','utf-8', function (error, source) {
+		  		if (error) {
+		  			require('../controllers/404').get(req, res);
+		  		}
+		  		res.writeHead(200, {
+		  			'Content-Type': 'text/html'
+		  		});
+				data['users'] = result.rows;
+		  		var template = handlebars.compile(source);
+		  		var html = template(data);
+		  		res.write(html);
+		  		res.end();
+		  	});
+
 		});
-		var template = handlebars.compile(source);
-		var html = template(data);
-		res.write(html);
-		res.end();
-	});
+	  });
+
+
 }
 
 /**********************************
